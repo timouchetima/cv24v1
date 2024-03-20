f3e52c5eea28ca341b300d411a73587c13c9bb13
bouriahi <fatibouriahi@gmail.com>
2024-03-20 15:33:22 +0100

    modification commit ajout mvnjson

--- src/main/java/fr/univrouen/cv24/Cv24v1Application.java
+++ src/main/java/fr/univrouen/cv24/Cv24v1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Cv24v1Application {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(Cv24v1Application.class, args);
 	}
 
